fa4ede70676b522e0de058e472e34d2b911b5606
Hafeez010 <c4052183@hallam.shu.ac.uk>
2024-11-06 09:26:24 +0000

    javas

--- js/myScript.js
+++ js/myScript.js
@@ -1,2 +1,14 @@
 //console.info("Hafeez Ahmed");
 document.getElementById('myHeading').innerHTML = "Hafeez Ahmed";
+document.querySelector('nav ul li').setAttribute('class', 'currentPage');
+document.querySelector('nav ul li a').setAttribute('href', 'http://www.google.co.uk');
+let colourButtons = document.querySelectorAll(".colPicker");
+console.dir(colourButtons);
+for (let i = 0; i < colourButtons.length,i++;)
+{
+    colourButtons[i].addEventListener("click", chgColour );
+}
+function chgColour(ev){
+    console.info(ev.target.classList[0]);
+    let chosenColour = ev.target.classList[0] + "Back"
+}
